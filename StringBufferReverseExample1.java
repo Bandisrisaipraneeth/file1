4697bbababeb595911a83fdb3da1577ef1e7aad3
Bandisrisaipraneeth <140943662+Bandisrisaipraneeth@users.noreply.github.com>
2023-09-17 20:21:53 +0530

    Add files via upload

---
+++ StringBufferReverseExample1.java
@@ -0,0 +1,9 @@
+class StringBufferReverseExample1
+ {  
+    public static void main(String[] args)
+ {  
+        StringBuffer sb = new StringBuffer("Srikanth_rathod");  
+        System.out.println("string: " + sb);  
+        System.out.println("reverse: " + sb.reverse());  
+    }  
+}  
